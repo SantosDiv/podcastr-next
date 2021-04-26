2883bc1431c6676cd9e2da4614931d56cd9c0c30
Diogenes <diogenesfelipe.s@outlook.com>
2021-04-25 21:34:55 -0300

    feat/Add title in page and favicon.png

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,6 +46,7 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import Head from 'next/head';
 import { api } from '../services/api';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
@@ -77,6 +78,10 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
   return (
     <div className={ styles.homePage }>
+
+      <Head>
+        <title>Home | Podcastr</title>
+      </Head>
       <section className={ styles.latestEpisodes }>
         <h2>Últimos lançamentos</h2>
         <ul>
